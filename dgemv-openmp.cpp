c5bfdbc76c16034cc8f13e6a3bc58280b05ce330
Issashihadeh <73568355+Issashihadeh@users.noreply.github.com>
2023-05-02 20:59:41 -0700

    Update dgemv-openmp.cpp

--- dgemv-openmp.cpp
+++ dgemv-openmp.cpp
@@ -16,9 +16,9 @@ void my_dgemv(int n, double* A, double* x, double* y) {
 
    #pragma omp parallel
    {
-      int nthreads = omp_get_num_threads();
-      int thread_id = omp_get_thread_num();
-      printf("Hello world: thread %d of %d checking in. \n", thread_id, nthreads);
+//       int nthreads = omp_get_num_threads();
+//       int thread_id = omp_get_thread_num();
+//       printf("Hello world: thread %d of %d checking in. \n", thread_id, nthreads);
    }
 
    // insert your dgemv code here. you may need to create additional parallel regions,
